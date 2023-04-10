dabc1b48c5afc4669d1548e9880fd1ecec3b66f5
moofox <130091962+moofox@users.noreply.github.com>
2023-04-10 11:53:54 +0000

    5-intergers

--- 5-Integers.js
+++ 5-Integers.js
@@ -6,38 +6,38 @@ describe('5-Integers.js', () => {
   // y el cero (sin decimales). Ejemplos: 0, 61, -15423142, 7
   it('se puede operar con ellos', () => {
     // ¡Sin calculadora!
-    expect(1 + 1).toBe(RellenameCorrectamente)
-    expect(1 - 1).toBe(RellenameCorrectamente)
-    expect(0 - 1).toBe(RellenameCorrectamente)
-    expect(12 * 4).toBe(RellenameCorrectamente)
-    expect(12 / 4).toBe(RellenameCorrectamente)
-    expect(12 / -4).toBe(RellenameCorrectamente)
-    expect(21 % 2).toBe(RellenameCorrectamente) // Este es el módulo, devuelve el resto de una división
-    expect(43 + 82 + 21).toBe(RellenameCorrectamente)
-    expect(14 + 57 * 100 / 2 - 100).toBe(RellenameCorrectamente)
+    expect(1 + 1).toBe(2)
+    expect(1 - 1).toBe(0)
+    expect(0 - 1).toBe(-1)
+    expect(12 * 4).toBe(48)
+    expect(12 / 4).toBe(3)
+    expect(12 / -4).toBe(-3)
+    expect(21 % 2).toBe(1) // Este es el módulo, devuelve el resto de una división
+    expect(43 + 82 + 21).toBe(146)
+    expect(14 + 57 * 100 / 2 - 100).toBe(2764 )
   })
   // Vale. El descanso se acabó.
   it('se pueden comparar', () => {
     // Cuando un entero es igual a otro es verdadero, si son diferentes, es falso.
     expect(10 === 10).toBe(true)
-    expect(10 === 20).toBe(RellenameCorrectamente)
+    expect(10 === 20).toBe(false)
     // ¿Te imaginas lo siguiente?
-    expect(20 !== 10).toBe(RellenameCorrectamente)
-    expect(20 !== 20).toBe(RellenameCorrectamente)
+    expect(20 !== 10).toBe(true)
+    expect(20 !== 20).toBe(false)
     // Se puede saber si uno es mayor/menor que el otro
-    expect(20 > 10).toBe(RellenameCorrectamente)
-    expect(10 > 20).toBe(RellenameCorrectamente)
-    expect(10 < 20).toBe(RellenameCorrectamente)
-    expect(20 < 10).toBe(RellenameCorrectamente)
+    expect(20 > 10).toBe(true)
+    expect(10 > 20).toBe(false)
+    expect(10 < 20).toBe(true)
+    expect(20 < 10).toBe(false)
     // Incluso si es igual o mayor/menor
-    expect(20 >= 10).toBe(RellenameCorrectamente)
-    expect(10 <= 20).toBe(RellenameCorrectamente)
-    expect(10 <= 10).toBe(RellenameCorrectamente)
+    expect(20 >= 10).toBe(true)
+    expect(10 <= 20).toBe(true)
+    expect(10 <= 10).toBe(true)
   })
 
   it('se pueden usar con operadores lógicos', () => {
-    expect((20 !== 10) && (10 <= 10)).toBe(RellenameCorrectamente)
-    expect((20 === 10) || (10 < 10)).toBe(RellenameCorrectamente)
+    expect((20 !== 10) && (10 <= 10)).toBe(true)
+    expect((20 === 10) || (10 < 10)).toBe(false )
   })
   // Este archivo ha sido más relajado ;)
 })
