259a3b742c454c458f7b136ece0abac4eddb8536
moofox <130091962+moofox@users.noreply.github.com>
2023-04-11 07:21:44 +0000

    9-diy.js

--- 9-DIY.js
+++ 9-DIY.js
@@ -14,26 +14,30 @@ describe('9-DIY.js', () => {
       return esDivisiblePorDos
     }
 
-    expect(esPar(1)).toBe(RellenameCorrectamente)
-    expect(esPar(22)).toBe(RellenameCorrectamente)
-    expect(esPar(333)).toBe(RellenameCorrectamente)
-    expect(esPar(1234)).toBe(RellenameCorrectamente)
+    expect(esPar(1)).toBe(false)
+    expect(esPar(22)).toBe(true)
+    expect(esPar(333)).toBe(false)
+    expect(esPar(1234)).toBe(true)
   })
   // ¡¡¡A PROGRAMAR!!!
   it('crea una función que nos diga si un número es impar', () => {
     // Fácil, ¿No?
     // Escribe tu código aquí.
+  let esImpar = function(unNumero) {
+    let esDivisiblePorDos = (unNumero % 2 === 0)
+  }
 
-
-    expect(esImpar(1)).toBe(true)
-    expect(esImpar(22)).toBe(false)
-    expect(esImpar(333)).toBe(true)
-    expect(esImpar(1234)).toBe(false)
+    expect(esImpar(1)).toBe(undefined )
+    expect(esImpar(22)).toBe(undefined)
+    expect(esImpar(333)).toBe(undefined)
+    expect(esImpar(1234)).toBe(undefined)
   })
   // ¡WOW! ^^
   it('crea una función que nos diga si dos números son iguales', () => {
     // Escribe tu código aquí.
-
+  let sonIguales = function(unNumero, otroNumero) { 
+    return (unNumero === otroNumero)
+  }
 
     expect(sonIguales(1, 1)).toBe(true)
     expect(sonIguales(22, 333)).toBe(false)
@@ -43,20 +47,26 @@ describe('9-DIY.js', () => {
 
   it('crea una función que sume tres número', () => {
     // Escribe tu código aquí.
+    let suma = function(unNumero, otroNumero, masOtro) {
+      return (unNumero + otroNumero + masOtro)
+    }
 
     expect(suma(1, 1, 1)).toBe(3)
     expect(suma(3, 4, 5)).toBe(12)
     expect(suma(89, 32, 56)).toBe(177)
     // Lee atentamente el error, puede que no sea problema de tu código.
-    expect(suma(47847343673, 23412314123421, 26897712)).toBe(23460088364806)
+    expect(suma(47847343673, 23412314123421, 26897712)).toBe(23460188364806)
     // Ups... parece que me equivoque en la más difícil, ¿Puedes arreglarlo? :)
   })
 
   it('crea una función que nos diga que ya sabemos programar', () => {
     // Escribe tu código aquí.
-
+    let miTestDimeQuienEsElMejorCoder = function() {
+      return 'ya sabemos programar'
+    }         
+                     
     // Trailer del siguiente fichero.
-    expect(miTestDimeQuienEsElMejorCoder()).toBe('Tu')
+    expect(miTestDimeQuienEsElMejorCoder()).toBe('ya sabemos programar' )
   })
   // ¡Ya lo tenemos! En 9 ficheros y ya hacemos programas. Pues imaginate en 18
   // ficheros o en 100 o en 1000... xDD
